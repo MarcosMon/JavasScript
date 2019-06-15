65294d28d1a5a31be1fbd830ed626df2c971a9c9
Marcos Monjon Mas <sysmarc13@hotmail.com>
2019-06-15 19:14:39 +0200

    Juego completo

---
+++ JuegoTesoro/js/script.js
@@ -0,0 +1,27 @@
+const WIDTH = 400;
+const HEIGH = 400;
+
+
+let target = {
+    x: getRandomNumber(WIDTH),
+    y: getRandomNumber(HEIGH)
+};
+
+
+let $map = document.getElementById('map');
+let $distance = document.getElementById('distance');
+let tocamientos = 0;
+
+$map.addEventListener('click',function(e) {
+    tocamientos++;
+    let distance = getDistance(e, target);
+    let distanceHint = getDistanceHint(distance);
+    $distance.innerHTML = `<h1>${distanceHint}</h1>`
+
+    if (distance < 20){
+        alert(`Me has tocado ${tocamientos} para conseguirlo Mmmmm`)
+        location.reload();
+    }
+    
+})
+
